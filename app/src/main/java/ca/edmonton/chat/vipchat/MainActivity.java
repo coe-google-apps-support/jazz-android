869d4eceee4d5e57277fbd9ec8d5f11e225fb001
Jared Rewerts <jarrew@COE.ADS>
2017-11-27 10:50:05 -0700

    Integrated Google login.

--- app/src/main/java/ca/edmonton/chat/vipchat/MainActivity.java
+++ app/src/main/java/ca/edmonton/chat/vipchat/MainActivity.java
@@ -6,24 +6,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.livechatinc.inappchat.ChatWindowActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
     }
 
-    public void login(View view) {
-        Log.i("Hola", "Logging in...");
-        Context context = view.getContext();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            launchChat(this, account.getDisplayName(), account.getEmail());
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            launchChat(this, account.getDisplayName(), account.getEmail());
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Whoopsie", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void launchChat(Context context, String name, String email) {
         Intent intent = new Intent(context, com.livechatinc.inappchat.ChatWindowActivity.class);
         intent.putExtra(ChatWindowActivity.KEY_GROUP_ID, "0");
         intent.putExtra(ChatWindowActivity.KEY_LICENCE_NUMBER, "9242305");
-        intent.putExtra(ChatWindowActivity.KEY_VISITOR_NAME, "Android Native");
-        intent.putExtra(ChatWindowActivity.KEY_VISITOR_EMAIL, "droid.native@email.com");
+        intent.putExtra(ChatWindowActivity.KEY_VISITOR_NAME, name);
+        intent.putExtra(ChatWindowActivity.KEY_VISITOR_EMAIL, email);
         context.startActivity(intent);
     }
 }
